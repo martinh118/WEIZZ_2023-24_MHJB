197263dd76c75fefd4a8d84389b19a4dd68b0e85
Martín H. Jaime Bonvin <94359581+martinh118@users.noreply.github.com>
2024-08-07 12:26:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
     <script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./SRC/CSS/welcome_page.css">
-    <script src="./controlador/controlador_welcome.js"></script>
+    <link rel="stylesheet" href="SRC/CSS/welcome_page.css">
+    <script src="controlador/controlador_welcome.js"></script>
     <title>Weizz - Welcome page</title>
 </head>
 <?php
-require_once("./controlador/config_proyecto/controlador_mostrar_proyectos.php");
+require_once("controlador/config_proyecto/controlador_mostrar_proyectos.php");
 session_start();
 ?>
 
@@ -20,12 +20,12 @@ session_start();
         <div class="row container-fluid" id="encabezado">
             <div class="col">
                 <a href="./index.php">
-                    <img src="./SRC/imagenes_web/logo_weizz_fondo.png" alt="logo_weizz" id="logo_weizz">
+                    <img src="SRC/imagenes_web/logo_weizz_fondo.png" alt="logo_weizz" id="logo_weizz">
                 </a>
 
             </div>
             <div class="col">
-                <h2 id="inicioPage"><a href="./index.php" class="tituloHeader">Inicio</a></h2>
+                <h2 id="inicioPage"><a href="index.php" class="tituloHeader">Inicio</a></h2>
             </div>
             <div class="col"></div>
 
@@ -33,19 +33,19 @@ session_start();
             if (isset($_SESSION['usuario'])) {
                 echo "<div class='col-4 offset-2'>
                 <h2 id='iniciarSesionPage'>
-                    <a href='./vista/config_usuario/configurar_usuario.php' class='tituloHeader'><b>" . $_SESSION['usuario'] . "</b></a>&nbsp;&nbsp;
-                    <a href='./controlador/forms/controlador_cerrar_sesion.php' class='tituloHeader'>Cerrar Sesión</a>
+                    <a href='vista/config_usuario/configurar_usuario.php' class='tituloHeader'><b>" . $_SESSION['usuario'] . "</b></a>&nbsp;&nbsp;
+                    <a href='controlador/forms/controlador_cerrar_sesion.php' class='tituloHeader'>Cerrar Sesión</a>
                 </h2>
             </div>";
             } else {
                 echo "<div class='col-2 offset-2'>
                 <h2 id='iniciarSesionPage'>
-                    <a href='./vista/forms/iniciar_sesion_form.php' class='tituloHeader'>Iniciar Sesión</a>
+                    <a href='vista/forms/iniciar_sesion_form.php' class='tituloHeader'>Iniciar Sesión</a>
                 </h2>
             </div>
             <div class='col-3'>
                 <h2 id='registroPage'>
-                    <a href='./vista/forms/registrar_form.php' class='tituloHeader'>Crear cuenta</a>
+                    <a href='vista/forms/registrar_form.php' class='tituloHeader'>Crear cuenta</a>
                 </h2>
 
             </div>";
@@ -67,9 +67,9 @@ session_start();
     <div class="row container-fluid">
 
         <div class="col-2 colNuevoProyecto">
-            <a href="./vista/seleccion_bases.php">
+            <a href="vista/seleccion_bases.php">
                 <div id="cuadroCrearProyecto">
-                    <img src="./SRC/imagenes_web/crear_plus.png" alt="crearProyecto" id="crearProyectoSimbol">
+                    <img src="SRC/imagenes_web/crear_plus.png" alt="crearProyecto" id="crearProyectoSimbol">
                 </div>
             </a>
             <h3 id="crearProyectoTitle"><b>Crear proyecto</b></h3>
@@ -123,7 +123,7 @@ session_start();
     <div class="modal fade" id="cambiarNombre" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-                <form action="./controlador/config_proyecto/controlador_cambiar_nombre.php" method="POST">
+                <form action="controlador/config_proyecto/controlador_cambiar_nombre.php" method="POST">
                     <div class="modal-body">
                         <h5>Escribe el nuevo nombre para tu proyecto: <br>
                             (25 char max)</h5>
